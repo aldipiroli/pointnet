08367f7475e479a5becb99e78df4aba8341d59c7
Aldi Piroli <aldi@MacBook-Pro.homenet.telecomitalia.it>
2021-08-21 11:25:56 +0200

    [ADD] Added modular implementation of the network

--- src/network.py
+++ src/network.py
@@ -78,6 +78,7 @@ class TNet3(nn.Module):
         x = self.fc2(x)
         x = self.fc3(x)
 
+        #  For stability
         iden = torch.eye(3, 3).repeat(batch_size, 1, 1)
         if x.is_cuda:
             iden = iden.to(self.device)
@@ -93,41 +94,33 @@ class TNet64(nn.Module):
         super(TNet64, self).__init__()
 
         self.device = device
-        self.K = 64
 
-        self.conv1 = nn.Conv1d(64, 64, 1)
-        self.conv2 = nn.Conv1d(64, 128, 1)
-        self.conv3 = nn.Conv1d(128, 1024, 1)
+        self.mlp1 = mlp(64, 64)
+        self.mlp2 = mlp(64, 128)
+        self.mlp3 = mlp(128, 1024)
 
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(128)
-        self.bn3 = nn.BatchNorm1d(1024)
-        self.bn4 = nn.BatchNorm1d(512)
-        self.bn5 = nn.BatchNorm1d(256)
-
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 4096)
-
-        self.dropout = nn.Dropout(p=0.7)
+        self.fc1 = fc(1024, 512)
+        self.fc2 = fc(512, 256, dropout=True)
+        self.fc3 = fc(256, 64 * 64)
 
     def forward(self, x):
         batch_size = x.shape[0]
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.mlp1(x)
+        x = self.mlp2(x)
+        x = self.mlp3(x)
 
         x = torch.max(x, 2)[0]
 
-        x = F.relu(self.bn4(self.fc1(x)))
-        x = self.dropout(F.relu(self.bn5(self.fc2(x))))
+        x = self.fc1(x)
+        x = self.fc2(x)
         x = self.fc3(x)
 
-        iden = torch.eye(self.K, self.K).repeat(batch_size, 1, 1)
+        #  For stability
+        iden = torch.eye(64, 64).repeat(batch_size, 1, 1)
         if x.is_cuda:
             iden = iden.to(self.device)
 
-        x = x.view(-1, self.K, self.K)
+        x = x.view(-1, 64, 64)
         x = x + iden
 
         return x
@@ -145,26 +138,14 @@ class PointNetClass(nn.Module):
         self.TNet3 = TNet3(self.device)
         self.TNet64 = TNet64(self.device)
 
-        self.mlp1 = nn.Conv1d(3, 64, 1)
-        self.mlp2 = nn.Conv1d(64, 64, 1)
-        self.mlp3 = nn.Conv1d(64, 64, 1)
-        self.mlp4 = nn.Conv1d(64, 128, 1)
-        self.mlp5 = nn.Conv1d(128, 1024, 1)
+        self.mlp1 = mlp(3, 64)
+        self.mlp2 = mlp(64, 64)
+        self.mlp3 = mlp(64, 128)
+        self.mlp4 = mlp(128, 1024)
 
-        self.mlp6 = nn.Conv1d(1024, 512, 1)
-        self.mlp7 = nn.Conv1d(512, 256, 1)
-        self.mlp8 = nn.Conv1d(256, self.k, 1)
-
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.bn3 = nn.BatchNorm1d(64)
-        self.bn4 = nn.BatchNorm1d(128)
-        self.bn5 = nn.BatchNorm1d(1024)
-
-        self.bn6 = nn.BatchNorm1d(512)
-        self.bn7 = nn.BatchNorm1d(256)
-
-        self.dropout = nn.Dropout(p=0.3)
+        self.mlp5 = mlp(1024, 512)
+        self.mlp6 = mlp(512, 256)
+        self.mlp7 = mlp(256, self.k, batchnorm=False)
 
     def forward(self, x):
         #  input transform:
@@ -173,8 +154,8 @@ class PointNetClass(nn.Module):
         x = torch.matmul(T3, x)
 
         #  mlp (64,64):
-        x = F.relu(self.bn1(self.mlp1(x)))
-        x = F.relu(self.bn2(self.mlp2(x)))
+        x = self.mlp1(x)
+        x = self.mlp2(x)
 
         # feature transform:
         x_ = x.clone()
@@ -183,15 +164,14 @@ class PointNetClass(nn.Module):
         x = torch.matmul(T64, x)
 
         #  mlp (64,128,1024):
-        x = F.relu(self.bn3(self.mlp3(x)))
-        x = F.relu(self.bn4(self.mlp4(x)))
-        x = F.relu(self.bn5(self.mlp5(x)))
+        x = self.mlp3(x)
+        x = self.mlp4(x)
 
         x = torch.max(x, 2, keepdim=True)[0]
 
-        x = F.relu(self.bn6(self.mlp6(x)))
-        x = F.relu(self.bn7(self.dropout(self.mlp7(x))))
-        x = self.mlp8(x)
+        x = self.mlp5(x)
+        x = self.mlp6(x)
+        x = self.mlp7(x)
 
         return x.squeeze()
 
@@ -208,30 +188,17 @@ class PointNetSeg(nn.Module):
         self.TNet3 = TNet3(self.device)
         self.TNet64 = TNet64(self.device)
 
-        self.mlp1 = nn.Conv1d(3, 64, 1)
-        self.mlp2 = nn.Conv1d(64, 64, 1)
-        self.mlp3 = nn.Conv1d(64, 64, 1)
-        self.mlp4 = nn.Conv1d(64, 128, 1)
-        self.mlp5 = nn.Conv1d(128, 1024, 1)
+        self.mlp1 = mlp(3, 64)
+        self.mlp2 = mlp(64, 64)
+        self.mlp3 = mlp(64, 64)
+        self.mlp4 = mlp(64, 128)
+        self.mlp5 = mlp(128, 1024)
 
-        #  segmentation part
-        self.mlp6 = nn.Conv1d(1088, 512, 1)
-        self.mlp7 = nn.Conv1d(512, 256, 1)
-        self.mlp8 = nn.Conv1d(256, 128, 1)
-        self.mlp9 = nn.Conv1d(128, self.m, 1)
+        self.mlp6 = mlp(1088, 512)
+        self.mlp7 = mlp(512, 256)
+        self.mlp8 = mlp(256, 128)
+        self.mlp9 = mlp(128, self.m, batchnorm=False)
 
-        self.bn1 = nn.BatchNorm1d(64)
-        self.bn2 = nn.BatchNorm1d(64)
-        self.bn3 = nn.BatchNorm1d(64)
-        self.bn4 = nn.BatchNorm1d(128)
-        self.bn5 = nn.BatchNorm1d(1024)
-
-        # segmentation part:
-        self.bn6 = nn.BatchNorm1d(512)
-        self.bn7 = nn.BatchNorm1d(256)
-        self.bn8 = nn.BatchNorm1d(128)
-
-        self.dropout = nn.Dropout(p=0.3)
 
     def forward(self, x):
         #  input transform:
@@ -240,8 +207,8 @@ class PointNetSeg(nn.Module):
         x = torch.matmul(T3, x)
 
         #  mlp (64,64):
-        x = F.relu(self.bn1(self.mlp1(x)))
-        x = F.relu(self.bn2(self.mlp2(x)))
+        x = self.mlp1(x)
+        x = self.mlp2(x)
 
         # feature transform:
         x_ = x.clone()
@@ -252,20 +219,19 @@ class PointNetSeg(nn.Module):
         x_feature = x.clone()
 
         #  mlp (64,128,1024):
-        x = F.relu(self.bn3(self.mlp3(x)))
-        x = F.relu(self.bn4(self.mlp4(x)))
-        x = F.relu(self.bn5(self.mlp5(x)))
-
+        x = self.mlp3(x)
+        x = self.mlp4(x)
+        x = self.mlp5(x)
+        
         x_globfeat = torch.max(x, 2, keepdim=True)[0]
 
         #  Concatenate global and local features
         x_globfeat = x_globfeat.expand(-1, -1, x_feature.shape[2])
         x = torch.cat((x_feature, x_globfeat), dim=1)
 
-        x = F.relu(self.bn6(self.mlp6(x)))
-        x = F.relu(self.bn7(self.mlp7(x)))
-        x = F.relu(self.bn8(self.mlp8(x)))
-
+        x = self.mlp6(x)
+        x = self.mlp7(x)
+        x = self.mlp8(x)
         x = self.mlp9(x)
 
         return x
